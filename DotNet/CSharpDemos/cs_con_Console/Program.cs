c9e00e4aee39c0ce85f646b2034fdb4239eb9f72
Manoj Kumar Sharma <mailme@manojkumarsharma.com>
2022-08-10 15:15:11 +0530

    Adding the C# Demos code files.

---
+++ DotNet/CSharpDemos/cs_con_Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace cs_con_Console
+{
+    internal class Program
+    {
+        static private void GetName()
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name + "!  Nice to meet you!");
+            Console.WriteLine("Hi {0}! Nice to meet you!", name);
+        }
+
+        private void GetAge()
+        {
+            Console.Write("Enter age: ");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
+            Console.WriteLine("You are " + age.ToString() + " years old!");
+            Console.WriteLine("You are {0} years old!", age);
+            Console.WriteLine("input: {0}, age {1}", input, age);
+        }
+
+        static void Main(string[] args)
+        {
+
+            Program.GetName();
+
+            Console.WriteLine();
+
+            Program p = new Program();
+            p.GetAge();
+
+        }
+    }
+}
